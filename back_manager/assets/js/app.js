4309e32355cd5d4c095b9f467f431f4836b24220
maojingxin01 <359788699@qq.com>
2017-09-12 10:13:17 +0800

    后台管理框架  员工管理列表

--- back_manager/assets/js/app.js
+++ back_manager/assets/js/app.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     // 读取body data-type 判断是哪个页面然后执行相应页面方法，方法在下面。
     var dataType = $('body').attr('data-type');
     console.log(dataType);
@@ -7,20 +7,26 @@ $(function() {
             pageData[key]();
         }
     }
+    // 判断用户是否已有自己选择的模板风格
+    if (storageLoad('SelcetColor')) {
+        $('body').attr('class', storageLoad('SelcetColor').Color)
+    } else {
+        storageSave(saveSelectColor);
+        $('body').attr('class', 'theme-black')
+    }
+
     autoLeftNav();
-    $(window).resize(function() {
+    $(window).resize(function () {
         autoLeftNav();
         console.log($(window).width())
     });
 
-    //    if(storageLoad('SelcetColor')){
+    if (storageLoad('SelcetColor')) {
 
-    //     }else{
-    //       storageSave(saveSelectColor);
-    //     }
-})
-
-
+    } else {
+        storageSave(saveSelectColor);
+    }
+});
 // 页面数据
 var pageData = {
     // ===============================================
@@ -32,49 +38,54 @@ var pageData = {
             bInfo: false, //页脚信息
             dom: 'ti'
         });
-
     }
-}
+};
+// 风格切换
+$('.tpl-skiner-toggle').on('click', function () {
+    $('.tpl-skiner').toggleClass('active');
+});
+$('.tpl-skiner-content-bar').find('span').on('click', function () {
+    $('body').attr('class', $(this).attr('data-color'))
+    saveSelectColor.Color = $(this).attr('data-color');
+    // 保存选择项
+    storageSave(saveSelectColor);
+
+});
 // 侧边菜单开关
-
-
 function autoLeftNav() {
 
 
-            $('.tpl-header-switch-button').on('click', function () {
-                if ($('.left-sidebar').is('.active')) {
-                    if ($(window).width() > 1024) {
-                        $('.tpl-content-wrapper').removeClass('active');
-                    }
-                    $('.left-sidebar').removeClass('active');
-                } else {
+    $('.tpl-header-switch-button').on('click', function () {
+        if ($('.left-sidebar').is('.active')) {
+            if ($(window).width() > 1024) {
+                $('.tpl-content-wrapper').removeClass('active');
+            }
+            $('.left-sidebar').removeClass('active');
+        } else {
 
-                    $('.left-sidebar').addClass('active');
-                    if ($(window).width() > 1024) {
-                        $('.tpl-content-wrapper').addClass('active');
-                    }
-                }
-            })
-
-            if ($(window).width() < 1024) {
-                $('.left-sidebar').addClass('active');
-            } else {
-                $('.left-sidebar').removeClass('active');
+            $('.left-sidebar').addClass('active');
+            if ($(window).width() > 1024) {
+                $('.tpl-content-wrapper').addClass('active');
             }
         }
+    })
 
-
+    if ($(window).width() < 1024) {
+        $('.left-sidebar').addClass('active');
+    } else {
+        $('.left-sidebar').removeClass('active');
+    }
+}
 // 侧边菜单
 $('.sidebar-nav-sub-title').on('click', function () {
-            $(this).siblings('.sidebar-nav-sub').slideToggle(80)
-                .end()
-                .find('.sidebar-nav-sub-ico').toggleClass('sidebar-nav-sub-ico-rotate');
-        });
-
+    $(this).siblings('.sidebar-nav-sub').slideToggle(80)
+        .end()
+        .find('.sidebar-nav-sub-ico').toggleClass('sidebar-nav-sub-ico-rotate');
+});
 //iframe
-var getWindowWH = function(){
-    return ["Height","Width"].map(function(name){
-        return window["inner"+name] ||
-            document.compatMode === "CSS1Compat" && document.documentElement[ "client" + name ] || document.body[ "client" + name ]
+var getWindowWH = function () {
+    return ["Height", "Width"].map(function (name) {
+        return window["inner" + name] ||
+            document.compatMode === "CSS1Compat" && document.documentElement["client" + name] || document.body["client" + name]
     });
-}
+};
