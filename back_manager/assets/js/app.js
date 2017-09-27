71c3049e79598e8bdb2dc8f32c43c70c7c91b1df
maojingxin01 <359788699@qq.com>
2017-09-27 11:39:56 +0800

    后台管理1.0

--- back_manager/assets/js/app.js
+++ back_manager/assets/js/app.js
@@ -45,9 +45,9 @@ $('.tpl-skiner-toggle').on('click', function () {
     $('.tpl-skiner').toggleClass('active');
 });
 $('.tpl-skiner-content-bar').find('span').on('click', function () {
-    $('body').attr('class', $(this).attr('data-color'))
-    saveSelectColor.Color = $(this).attr('data-color');
+    $('body').attr('class', $(this).attr('data-color'));
     // 保存选择项
+    saveSelectColor.Color = $(this).attr('data-color');
     storageSave(saveSelectColor);
 
 });
